65be401ab1a63c3fa90fc7e52530a6e5a698e0f2
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-25 15:49:16 +0200

    Refactor app.js to add user routes and improve code structure

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/userRoute")
+const PORT = process.env.PORT || 8082
 
-main().catch(err=> console.log(err))
+main().catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect(process.env.CONNECTION_STRING)
+  await mongoose.connect(process.env.CONNECTION_STRING);
 }
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use("/users", userRouter)
+
+app.listen(PORT,()=>console.log(`The app is live at http://localhost:${PORT}`))
+
+
