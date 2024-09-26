2d7dd9a050a51c7331bd4a85051f0e8f5e3310dc
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-26 21:43:18 +0200

    Add socket.js for socket.io integration

---
+++ sockets/socket.js
@@ -0,0 +1,22 @@
+const socketIo = require('socket.io')
+
+let io;
+
+module.exports = {
+    init:(server)=>{
+        io = socketIo(server, {
+            cors: {
+                origin: "http://localhost:5173"
+            }
+        })
+        return io;
+    },
+
+    getIo: ()=>{
+        if(!io){
+            throw new Error('Socket.io not initialized')
+
+        }
+        return io;
+    }
+}
