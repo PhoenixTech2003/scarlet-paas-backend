3887504787547023c6e3ff3b0d347dfefe802948
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-26 09:04:18 +0200

    Add CORS middleware and webhooks route

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/userRoute")
+const cors = require("cors")
+const webhhooksRouter = require("./routes/webhooksRoute")
+
 const PORT = process.env.PORT || 8082
 
 main().catch((err) => console.log(err));
@@ -14,8 +17,10 @@ const app = express();
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(cors())
 
 app.use("/users", userRouter)
+app.use("/api/webhooks", webhhooksRouter)
 
 app.listen(PORT,()=>console.log(`The app is live at http://localhost:${PORT}`))
 
