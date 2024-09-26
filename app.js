d18ab5f8e8664ee7d8f8ffe8225f6e6f757f4aa3
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-26 11:44:14 +0200

    Add route for user deployments

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const userRouter = require("./routes/userRoute")
 const cors = require("cors")
 const webhhooksRouter = require("./routes/webhooksRoute")
+const deploymentsRouter = require("./routes/deploymentsRoute")
 
 const PORT = process.env.PORT || 8082
 
@@ -21,6 +22,7 @@ app.use(cors())
 
 app.use("/users", userRouter)
 app.use("/api/webhooks", webhhooksRouter)
+app.use("/deployments", deploymentsRouter)
 
 app.listen(PORT,()=>console.log(`The app is live at http://localhost:${PORT}`))
 
