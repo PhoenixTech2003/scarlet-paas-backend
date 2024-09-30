fd5b07682db2e97d7b17da88c3bd5384986ccfc1
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-30 06:47:04 +0200

    Refactor create_user_directory.sh: Create logs directory for user ID

--- bash_scripts/create_user_directory.sh
+++ bash_scripts/create_user_directory.sh
@@ -9,8 +9,10 @@ fi
 # Assign the arguments to variables
 USER_ID=$1
 DIRECTORY_PATH=$2
+LOGS_PATH=$3
 
 # Create a directory with the user ID as the name
 mkdir -p "$DIRECTORY_PATH/$USER_ID"
+mkdir -p "$LOGS_PATH/$USER_ID"
 
 echo "Directory created for user ID: $USER_ID at $DIRECTORY_PATH"
