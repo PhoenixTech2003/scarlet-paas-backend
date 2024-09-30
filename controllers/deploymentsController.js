aaef155b38a2fdb2b41cc9433452e829732df5cb
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-30 06:47:15 +0200

    Refactor deploymentsController.js: Add SQLite database integration for logging

--- controllers/deploymentsController.js
+++ controllers/deploymentsController.js
@@ -4,6 +4,8 @@ const path = require("node:path");
 const { exec } = require("node:child_process");
 const socket = require("../sockets/socket");
 const Deployments = require("../models/deployment");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -27,7 +29,7 @@ exports.deploymentsPost = [
   asyncHandler(async (req, res) => {
     const id = req.body.id;
     const file = req.file;
-    let logs = []
+    let logs = [];
 
     if (!file) {
       return res.status(400).send({ message: "No file uploaded" });
@@ -37,22 +39,61 @@ exports.deploymentsPost = [
       app_name: req.body.appname,
       userId: req.body.id,
       status: "pending",
+      createdAt: new Date(),
     });
 
-   const deploymentDetails =  await appDeployemnt.save();
+    const deploymentDetails = await appDeployemnt.save();
 
-   const deploymentId = deploymentDetails._id.toString()
-   
+    const deploymentId = deploymentDetails._id.toString();
+
+    const pathToDb = path.join(
+      __dirname,
+      `../logs/${req.body.id}/${deploymentId}`
+    );
+
+    const db = await open({
+      filename: pathToDb,
+      driver: sqlite3.Database,
+    });
+
+    await db.exec(`
+    CREATE TABLE IF NOT EXISTS logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        cleint_offset TEXT UNIQUE,
+        content TEXT
+    );
+    `);
+
+    let result;
 
     const pathToScript = path.join(__dirname, "../bash_scripts/deployment.sh");
-    console.log(req.body.appname, file.filename)
-    const pathToFile = path.join(__dirname, `../users/${id}/${req.body.appname + path.extname(file.filename)}`);
+    console.log(req.body.appname, file.filename);
+    const pathToFile = path.join(
+      __dirname,
+      `../users/${id}/${req.body.appname + path.extname(file.filename)}`
+    );
     const pathToDestination = path.join(__dirname, `../users/${id}`);
     const script = exec(`${pathToScript} ${pathToFile} ${pathToDestination}`);
 
-    script.stdout.on("data", (data) => {
+    script.stdout.on("data", async (data) => {
       console.log(`stdout: ${data}`);
-      logs.push({type:"stdout", message:data.toString()});
+      logs.push(data.toString());
+      result = await db.run(
+        "INSERT INTO logs (content) VALUES (?)",
+        data.toString()
+      );
+      socket
+        .getIo()
+        .emit(`${req.body.id}-${deploymentId}`, logs, result.lastID);
+      if (!socket.recovered) {
+        await db.each(
+          "SELECT id, content FROM  logs WHERE id > ?",
+          [socket.serverOffset ],
+          (_err, row) => {
+            console.log(row.content, row.id);
+          }
+        );
+      }
     });
 
     script.stderr.on("data", (data) => {
@@ -69,14 +110,52 @@ exports.deploymentsPost = [
         );
         return res.status(500).send({ message: "Error extracting file" });
       }
-     await Deployments.updateOne(
+      await Deployments.updateOne(
         { app_name: req.body.appname },
         { $set: { status: "Running" } }
       );
-      socket.getIo().emit("log", logs)
+
       res
         .status(200)
-        .json({message:"deployment was successful", deploymentId, userId: req.body.id});
+        .json({
+          message: "deployment was successful",
+          deploymentId,
+          userId: req.body.id,
+        });
     });
   }),
 ];
+
+exports.deploymentsGet = asyncHandler(async (req, res) => {
+  try {
+    const userDeployements = await Deployments.find({
+      userId: req.params.userId,
+    }).sort({ createdAt: -1 });
+  
+    res.status(200).json(userDeployements);
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: `an error occured while getting deployments ${error.message}`,
+      });
+  }
+});
+
+exports.deploymentsGetId = asyncHandler(async(req, res)=>{
+  const logs = []
+  const pathToDb = path.join(__dirname,`../logs/${req.params.userId}/${req.params.deploymentId}`)
+  const db = await open({
+    filename: pathToDb,
+    driver: sqlite3.Database
+  })
+
+  await db.each(
+    "SELECT * FROM logs",
+    (_err, row) => {
+      logs.push(row.content);
+    })
+
+    res.status(200).send(logs)
+  
+})
