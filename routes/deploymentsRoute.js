c8ec30626e6484a8c62b7feba4df4d50310f6604
Chiyembekezo <phoenixtech2003@gmail.com>
2024-09-30 06:48:29 +0200

    Refactor deploymentsRoute.js: Add GET endpoints for deployment retrieval

--- routes/deploymentsRoute.js
+++ routes/deploymentsRoute.js
@@ -4,5 +4,7 @@ const deploymentsController = require("../controllers/deploymentsController")
 const deploymentsRouter = Router()
 
 deploymentsRouter.post('/', deploymentsController.deploymentsPost)
+deploymentsRouter.get('/:userId/:deploymentId', deploymentsController.deploymentsGetId)
+deploymentsRouter.get('/:userId',deploymentsController.deploymentsGet)
 
 module.exports = deploymentsRouter
